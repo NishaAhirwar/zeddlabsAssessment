7d888f34770a896e01371c18bdadff4781eae8fe
NishaAhirwar <nishaahirwar.work@gmail.com>
2023-06-09 02:57:17 +0530

    third commit

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,7 +4,7 @@ import CountContext from "../context/context";
  const Counter = () => {
     let {count, setCount} = useContext(CountContext);
   return (
-    <div className='container-odd'>
+    <div className='Counter'>
         <h1>Counter Component</h1>
         <button className='btn' onClick={()=>setCount(count++)}>increment</button>
         <button className='btn' onClick={()=>setCount(count--)}>decrement</button>
